693d0606e725ba2846276a821d9070f8bd901a0d
alimacosen <caifanglei1998@gmail.com>
2023-05-02 18:50:37 -0700

    finish initial character design

---
+++ characters/design/design.go
@@ -0,0 +1,122 @@
+package design
+
+import (
+	. "goa.design/goa/v3/dsl"
+)
+
+var _ = API("charactersAPI", func() {
+	Title("Characters Service")
+	Description("Service for characters management")
+	Server("charactersServer", func() {
+		Host("localhost", func() {
+			URI("grpc://localhost:8060")
+		})
+	})
+})
+
+var Character = Type("Character", func() {
+	Field(1, "id", String, "UUId of the character", func() {
+		Meta("rpc:tag", "1")
+	})
+	Field(2, "name", String, "Name of the character", func() {
+		Meta("rpc:tag", "2")
+	})
+	Field(3, "description", String, "Description of the character", func() {
+		Meta("rpc:tag", "3")
+	})
+	Field(4, "health", Int, "The amount of health value of the character", func() {
+		Meta("rpc:tag", "4")
+	})
+	Field(5, "experience", Int, "The amount of experience value of the character", func() {
+		Meta("rpc:tag", "5")
+	})
+	Field(6, "inventoryId", String, "UUId of the inventory record", func() {
+		Meta("rpc:tag", "6")
+	})
+	Required("id", "name", "description", "health", "experience", "inventoryId")
+})
+
+var _ = Service("CharacterService", func() {
+	Description("The character service performs CRUD operations for characters and their attributes.")
+
+	Method("createCharacter", func() {
+		Payload(func() {
+			Field(1, "name", String, "Name of the Character", func() {
+				Meta("rpc:tag", "1")
+			})
+			Field(2, "description", String, "Description of the Character", func() {
+				Meta("rpc:tag", "2")
+			})
+			Required("name")
+		})
+		Result(Character)
+		Error("invalid_args", String, "Invalid arguments. Required: name  Optional: description ")
+		GRPC(func() {
+			Response(CodeOK)
+			Response("invalid_args", CodeInvalidArgument)
+		})
+	})
+
+	Method("getCharacter", func() {
+		Payload(func() {
+			Field(1, "id", String, "UUId of the Character", func() {
+				Meta("rpc:tag", "1")
+			})
+			Required("id")
+		})
+		Result(Character)
+		Error("invalid_args", String, "Invalid arguments. Required: id ")
+		Error("no_match", String, "No character matched given criteria")
+		GRPC(func() {
+			Response(CodeOK)
+			Response("invalid_args", CodeInvalidArgument)
+			Response("no_match", CodeNotFound)
+		})
+	})
+
+	Method("updateCharacterAttributes", func() {
+		Payload(func() {
+			Field(1, "id", String, "UUId of the Character", func() {
+				Meta("rpc:tag", "1")
+			})
+			Field(2, "name", String, "Name of the Character", func() {
+				Meta("rpc:tag", "2")
+			})
+			Field(3, "description", String, "Description of the Character", func() {
+				Meta("rpc:tag", "3")
+			})
+			Field(4, "health", Int, "The amount of health value of the Character", func() {
+				Meta("rpc:tag", "4")
+			})
+			Field(5, "experience", Int, "The amount of experience value of the Character", func() {
+				Meta("rpc:tag", "5")
+			})
+			Required("id")
+		})
+		Result(Character)
+		Error("invalid_args", String, "Invalid arguments. Required: id  Optional: name, description, health, experience ")
+		Error("no_match", String, "No character matched given criteria")
+		GRPC(func() {
+			Response(CodeOK)
+			Response("invalid_args", CodeInvalidArgument)
+			Response("no_match", CodeNotFound)
+		})
+	})
+
+	Method("deleteCharacter", func() {
+		Payload(func() {
+			Field(1, "id", String, "UUId of the Character", func() {
+				Meta("rpc:tag", "1")
+			})
+			Required("id")
+		})
+		Error("invalid_args", String, "Invalid arguments. Required: id ")
+		Error("no_match", String, "No character matched given criteria")
+		Result(Character)
+		GRPC(func() {
+			Response(CodeOK)
+			Response("invalid_args", CodeInvalidArgument)
+			Response("no_match", CodeNotFound)
+		})
+	})
+})
